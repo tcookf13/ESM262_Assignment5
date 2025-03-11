506912f8f58ad227dde9b024269181a24c817916
kelsey-warren <kelseywarren8@gmail.com>
2025-03-11 10:12:21 -0700

    basically just added R script for oil function

---
+++ R/oil.R
@@ -0,0 +1,13 @@
+
+#' Function title
+#'
+#' function purpose
+#' @param 
+#' @return 
+#' @examples
+#' compute_simpson_index(c("butterfly","butterfly","mosquito","butterfly","ladybug","ladybug")))
+#' @references
+#' http://www.tiem.utk.edu/~gross/bioed/bealsmodules/simpsonDI.html
+
+
+
